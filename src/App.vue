4095f942120190a608bc49be02b0bfd860f650af
erickcruzf <erickcruz_57@hotmail.com>
2022-05-19 21:33:43 -0300

    Fim Wallets

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="app">
+    <div id="left-header" v-if="isAuth" class="float-right">
+      <span class="float-left pl-3 pt-1">Olá, {{this.$store.state.auth.user.firstName}} {{this.$store.state.auth.user.lastName}}</span>
+      <span id="spLogout" class="float-right" @click="logout">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-box-arrow-right logoutIcon" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+          <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+        </svg>
+      </span>
+      <b-tooltip target="spLogout" title="Tooltip title" triggers="hover">
+        Encerrar Sessão.
+      </b-tooltip>
+    </div>
     <div id="header" @click="goHome">
       <img id="letra" alt="Safi Letra" src="./assets/safiLetra.png">
       <router-link to="/"></router-link>
@@ -15,6 +27,11 @@ export default {
   name: 'App',
   components: {
   },
+  computed: {
+    isAuth() {
+      return this.$store.state.auth.status.loggedIn;
+    }
+  },
   mounted() {
   },
   methods: {
@@ -27,6 +44,13 @@ export default {
           console.log(err);
         }
       });
+    },
+    logout() {
+      this.$store.dispatch('auth/logout').then(
+            () => {
+              this.$router.push('/');
+            }
+          );
     }
   }
 }
@@ -51,6 +75,18 @@ export default {
   border-start-end-radius: 0;
   border-bottom-left-radius: 0;
   border-top-left-radius: 0;
+}
+
+#left-header {
+  width: 100%;
+  display: block;
+  min-height: 1.75rem;
+  background-color: #2c3e50;
+  color: #F2F2F2;
+  width: 20rem;
+  border-radius: 1rem;
+  margin-top: 2px;
+  margin-right: 2px;
 }
 
 #body {
@@ -79,4 +115,15 @@ export default {
     white-space: pre-wrap;
     overflow: auto;
 }
+
+.logoutIcon {
+  padding-right: 5px;
+}
+
+.logoutIcon:hover {
+  padding-right: 5px;
+  color: #111111;
+  cursor: pointer;
+}
+
 </style>
--- src/views/AreaLogada.vue
+++ src/views/AreaLogada.vue
@@ -4,23 +4,31 @@
             <div v-for="(wallet,index2) in walletList" :key="index2">
                 <b-card @click="addOrUpdateWallet(wallet)"
                     border-variant="success"
-                    :header="wallet.name"
                     header-bg-variant="#2c3e50"
                     header-class= "walletHeader"
                     class="wallet my-2"
                     header-text-variant="white"
                     align="center"
                     body-class="walletBody">
-                    <b-card-text v-if="wallet.name != 'Nova Carteira'">Maiores Posições
-                        <ul>
-                            <li>
-                                Bitcoin 39%;
-                            </li>
-                            <li>
-                                Ethereum 39%;
-                            </li>
-                            <li>
-                                Luna 23%;
+                    <template #header>
+                      <span :id="'spDelete_' + index + '_' + index2" class="float-right" @click.stop.prevent="showConfirmation(wallet)">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash2-fill align-baseline" viewBox="0 0 16 16">
+                          <path d="M2.037 3.225A.703.703 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2a.702.702 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225zm9.89-.69C10.966 2.214 9.578 2 8 2c-1.58 0-2.968.215-3.926.534-.477.16-.795.327-.975.466.18.14.498.307.975.466C5.032 3.786 6.42 4 8 4s2.967-.215 3.926-.534c.477-.16.795-.327.975-.466-.18-.14-.498-.307-.975-.466z"/>
+                        </svg>
+                      </span>
+                      <b-tooltip :target="'spDelete_' + index + '_' + index2" title="Tooltip title" triggers="hover">
+                        Deletar Carteira.
+                      </b-tooltip>
+                      <h6 class="mb-0">{{wallet.name}}</h6>
+                    </template>
+                    <b-card-text v-if="wallet.name != 'Nova Carteira'">
+                        <div class="py-4 text-center" v-if="!wallet.walletCurrency[0]">
+                          Carteira Vazia.
+                        </div>
+                        <ul v-else>
+                            Maiores Posições
+                            <li v-for="(coin,index3) in formatHoldings(wallet)" :key="index3">
+                                {{coin.currency.name}} {{coin.percent.toFixed(2)}}%
                             </li>
                         </ul>
                     </b-card-text>
@@ -33,7 +41,7 @@
                         <b-button id="buttonCriarCarteira" variant="outline-primary" class="w-25 inputsDinamicos buttonMain" @click="confirmarNovaCarteira">Confirmar</b-button>
                       </div>
                     </div>
-                    <div class="text-center py-2" v-else>
+                    <div class="text-center py-4" v-else>
                       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
                       </svg>
@@ -41,6 +49,13 @@
                 </b-card>
             </div>
         </b-card-group>
+        <b-modal ref="confirmationModal" hide-footer hide-header>
+          <div class="d-block text-center">
+            <h3>Tem certeza que deseja deletar essa carteira?.</h3>
+          </div>
+          <b-button class="mt-3" variant="outline-danger" block @click="cancelDelete">Cancelar</b-button>
+          <b-button class="mt-3" variant="outline-success" block @click="confirmDelete">Sim</b-button>
+        </b-modal>
     </div>
 </template>
 
@@ -56,8 +71,9 @@ import authHeader from '../services/auth-header';
         showInputCarteira: false,
         stateCadastro: {Nome: null, Sobrenome: null, Email: null},
         loading: false,
-        allWallets: [{name: "Primeira Carteira"}, {name: "sapo"}, {name: "poder"}],
-        loadingWallets: false
+        allWallets: [],
+        loadingWallets: false,
+        walletDelete: {}
       }
     },
     created() {
@@ -117,9 +133,9 @@ import authHeader from '../services/auth-header';
     },
     carregarCarteiras() {
       this.loadingWallets = true;
-      axios.get(`wallet/${this.$store.state.auth.user.id}`, { headers: authHeader() })
+      axios.get(`wallet/${this.$store.state.auth.user.id}/userId`, { headers: authHeader() })
         .then(response => {
-            this.allWallets = response;
+          this.allWallets = response.data;
         })
         .catch((error) => {
           console.log(error);
@@ -127,6 +143,42 @@ import authHeader from '../services/auth-header';
         .finally(() => {
           this.loadingWallets = false;
         });
+    },
+    formatHoldings(wallet) {
+      var totalAux = 0;
+      wallet.walletCurrency.forEach(coin => {
+        coin.totalValue = coin.averagePrice * coin.quantity;
+        totalAux += coin.totalValue;
+      });
+      wallet.walletCurrency.forEach(coin => {
+        coin.percent = ((coin.totalValue * 100) / totalAux);
+      });
+      
+      return wallet.walletCurrency.slice().sort((x, y) => x.percent < y.percent ? 1 : -1).slice(0, 3);
+    },
+    deletarCarteira() {
+      axios.delete(`wallet/${this.walletDelete.id}`, { headers: authHeader() })
+        .then(() => {
+          this.allWallets.splice(this.allWallets.findIndex(x => x.id == this.walletDelete.id), 1);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          this.loadingWallets = false;
+        });
+    },
+    showConfirmation(wallet) {
+      this.$refs['confirmationModal'].show();
+      this.walletDelete = wallet;
+    },
+    cancelDelete() {
+      this.$refs['confirmationModal'].hide();
+      this.walletDelete = {};
+    },
+    confirmDelete() {
+      this.$refs['confirmationModal'].hide();
+      this.deletarCarteira();
     }
   },
   computed: {
@@ -138,7 +190,7 @@ import authHeader from '../services/auth-header';
         this.allWallets.forEach(wallet => {
             aux ++;
             newLista.push(wallet);
-
+            
             if (aux == this.allWallets.length) {
                 if (newLista.length < 3) {
                     newLista.push(newWallet);
@@ -180,7 +232,7 @@ import authHeader from '../services/auth-header';
 .wallet {
   min-width: 12rem;
   max-width: 25rem;
-  min-height: 10rem;
+  min-height: 13rem;
 }
 
 .wallet:hover {
