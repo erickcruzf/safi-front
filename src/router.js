c32c168c42a54f9f3c6f4597472ca08c2a96515f
erickcruzf <erickcruz_57@hotmail.com>
2022-05-22 20:40:01 -0300

    verify fix

--- src/router.js
+++ src/router.js
@@ -40,8 +40,8 @@ const router = new Router({
 // eslint-disable-next-line
 router.beforeEach((to, from, next) => {
     console.log("To: " + to.name);
-    if (to.name !== 'login' && !store.state.auth.status.loggedIn) next({ name: 'login' })
-    if (to.name === 'login' && to.name !== 'validate' && store.state.auth.status.loggedIn) next({ name: 'home' })
+    if (to.name !== 'login' && to.name !== 'validate' && !store.state.auth.status.loggedIn) next({ name: 'login' })
+    if (to.name === 'login' && store.state.auth.status.loggedIn) next({ name: 'home' })
     else next()
     next();
 })
